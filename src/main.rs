5d0cd722f8c0f9e6abbbf844b2c11cc06952d337
copperthief <copperthiefcorvid@gmail.com>
2024-02-02 00:55:32 -0500

    vulkan instance creation and destruction

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,30 @@ use winit::{
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
-use vulkanalia::*;
 use vulkanalia::prelude::v1_2::*;
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
 
 fn main() {
 
-    let entry: Entry;
-    unsafe { entry = Entry::new(loader::LibloadingLoader(loader::LIBRARY)).map_err(|e| format!("{e}"))?; }
-    
-    let app_info = vk::ApplicationInfo::builder()
-        .application_name(b"deez\0")
-        .application_version(vk::make_version(0, 0, 0))
-        .build();
-    
-    let instance_info = vk::InstanceCreateInfo::builder()
-        .application_info(&app_info)
-        .build();
+    let instance: Instance;
 
-    let instance = entry.create_instance(&instance_info);
+    unsafe {
+        let loader = LibloadingLoader::new(LIBRARY).unwrap();
+        let entry = Entry::new(loader).unwrap();
+
+        let app_info = vk::ApplicationInfo::builder()
+            .application_name(b"deez\0")
+            .application_version(vk::make_version(0, 0, 0))
+            .build();
+    
+        let instance_info = vk::InstanceCreateInfo::builder()
+            .application_info(&app_info)
+            .build();
+
+        instance = entry.create_instance(&instance_info, None).unwrap();
+    }
+
+    
 
     let event_loop = EventLoop::new().unwrap();
     let window = WindowBuilder::new()
@@ -43,4 +49,8 @@ fn main() {
             _ => ()
         }
     }).unwrap();
+
+
+
+    unsafe { instance.destroy_instance(None); }
 }
