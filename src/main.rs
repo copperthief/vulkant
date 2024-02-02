948707352071511bb3fd16dddf625e7d0994ad90
copperthief <copperthiefcorvid@gmail.com>
2024-02-01 23:41:37 -0500

    device change

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,25 @@ use winit::{
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
+use vulkanalia::*;
+use vulkanalia::prelude::v1_2::*;
 
 fn main() {
+
+    let entry: Entry;
+    unsafe { entry = Entry::new(loader::LibloadingLoader(loader::LIBRARY)).map_err(|e| format!("{e}"))?; }
+    
+    let app_info = vk::ApplicationInfo::builder()
+        .application_name(b"deez\0")
+        .application_version(vk::make_version(0, 0, 0))
+        .build();
+    
+    let instance_info = vk::InstanceCreateInfo::builder()
+        .application_info(&app_info)
+        .build();
+
+    let instance = entry.create_instance(&instance_info);
+
     let event_loop = EventLoop::new().unwrap();
     let window = WindowBuilder::new()
         .with_title("bird window (if not bird, do not look)")
